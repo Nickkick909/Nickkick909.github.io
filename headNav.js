2e1aa7394156ddd035ab0230d2b5364fce25e4f4
Nicholas Knight <38379829+Nickkick909@users.noreply.github.com>
2020-04-03 11:48:06 -0400

    attempting to add lit-element

---
+++ headNav.js
@@ -0,0 +1,13 @@
+import { LitElement , html } from 'lit-element';
+
+export class headNav extends LitElement {
+
+    render() {
+        return html`
+        <p>Test</p>
+        
+        `;
+    }
+}
+
+customElements.define('headNav', headNav);
