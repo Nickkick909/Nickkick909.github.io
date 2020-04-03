3e203e55f74fd77da19441f7259653737e5d0ee5
Nicholas Knight <38379829+Nickkick909@users.noreply.github.com>
2020-04-03 11:56:38 -0400

    maybe this time

--- headNav.js
+++ headNav.js
@@ -1,11 +1,26 @@
-import { LitElement , html } from 'lit-element';
+import { LitElement , html } from './node_modules/lit-element';
 
 export class headNav extends LitElement {
 
     render() {
         return html`
-        <p>Test</p>
-        
+        <span>
+            <p class ="name">
+                Lab Lineup
+            </p>
+        </span>
+        <hr>
+        <nav>
+            <a class ="navbar" href="/">Home</a> |
+            <a class ="navbar" href="LabLineup.html">Lab Lineup</a> |
+            <a class ="navbar" href="PacmanProjects.html">Pacman Projects</a> |
+            <a class ="navbar" href="GamerGuru.html">Gamer Guru</a> |
+            <a class ="navbar" href="DodgeCar.html">Dodge Car</a> |
+            <a class ="navbar" href="Pong.html">Pong</a> |
+    
+        </nav>
+
+        <br>
         `;
     }
 }
