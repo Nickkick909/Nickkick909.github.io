24270c102167c33e4c95dc45db6896c16c2f3830
Nicholas Knight <nicholasknight1999@yahoo.com>
2024-09-14 00:51:59 -0400

    Switching to itch.io for game hosting

--- src/pages/OtterAdventures.js
+++ src/pages/OtterAdventures.js
@@ -1,51 +1,6 @@
-import React, { useEffect, useState } from "react";
-import {  useNavigate } from 'react-router-dom';
+import React from "react";
 
-// import axios from "axios";
-// import {
-//   Box,
-//   Typography,
-//   Button,
-//   Grid,
-//   Card,
-//   Divider,
-//   Modal
-// } from '@mui/material';
-
-import { Unity, useUnityContext } from "react-unity-webgl";
-
-import DeviceEmulator from 'react-device-emulator';
-import 'react-device-emulator/lib/styles/style.css';
-
-// import {
-//   User,
-//   Clock,
-//   XCircle,
-//   Package
-// } from 'react-feather';
-
-// import MenuBookIcon from '@mui/icons-material/MenuBook';
 function OtterAdventures() {
-  const [unityGame, setUnityGame] = useState("OtterAdventures")
-
-    const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
-        loaderUrl: `${unityGame}/Build/${unityGame}.loader.js`,
-        dataUrl: `${unityGame}/Build/${unityGame}.data`,
-        frameworkUrl: `${unityGame}/Build/${unityGame}.framework.js`,
-        codeUrl: `${unityGame}/Build/${unityGame}.wasm`,
-      });
-
-      const loadingPercentage = Math.round(loadingProgression * 100);
-
-
-      
-
-    const navigate = useNavigate()
-
-    useEffect(() => {
-    
-     
-    }, []);
     
     return (
         <div className="">
@@ -60,14 +15,8 @@ function OtterAdventures() {
             <div className="description">
               Otter Adventures is my current project. The idea is to be a Pokemon style game where the main character is an otter. You will go around the world and fight other river animals. You will level up and increase your stats and learn new attacks.
             </div>
-            {isLoaded === false && (
-              // We'll conditionally render the loading overlay if the Unity
-              // Application is not loaded.
-              <div className="loading-overlay">
-                <p>Loading... ({loadingPercentage}%)</p>
-              </div>
-            )}
-              <Unity unityProvider={unityProvider} className="otterAdventures"/>
+
+              <iframe frameborder="0" src="https://itch.io/embed-upload/11468995?color=122157" allowfullscreen="" width="768" height="432"><a href="https://nrknight.itch.io/otter-adventures" className="otterAdventures">Play Otter Adventures on itch.io</a></iframe>
           </div>
       </div>
     
